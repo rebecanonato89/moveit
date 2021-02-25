528d8507f963f714c7bf306f6f4f00ce7919ec94
REBECANONATO <rebecanonato89@gmail.com>
2021-02-25 09:41:36 -0300

    Correções e implementações

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,12 +4,14 @@ import styles from '../styles/components/Countdown.module.css';
 
 
 let countdownTimeout: NodeJS.Timeout;
-export function Countdown() {
-    const { startNewChallenge } = useContext(ChallengesContext)
 
-    const [time, settime] = useState(5);
-    const [isActive, setActive] = useState(false);
-    const [Finished, setFinished] = useState(false);
+export function Countdown() {
+
+    const { startNewChallenge } = useContext(ChallengesContext);
+
+    const [time, setTime] = useState(0.10 * 60);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -18,28 +20,35 @@ export function Countdown() {
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown() {
-        setActive(true);
+        setIsActive(true);
     }
 
     function resetCountdown() {
         clearTimeout(countdownTimeout);
-        setActive(false);
-        settime(25 * 60);
+        setIsActive(false);
+        setTime(0.10 * 60);
     }
 
     useEffect(() => {
+
         if (isActive && time > 0) {
+
             countdownTimeout = setTimeout(() => {
-                settime(time - 1);
+                setTime(time - 1);
             }, 1000);
+
         } else if (isActive && time == 0) {
-            setFinished(true);
-            setActive(false);
+            setHasFinished(true);
+            setIsActive(false);
             startNewChallenge();
         }
-    }, [isActive, time])
+    }, [isActive, time]);
+
+
+
     return (
         <div>
+
             <div className={styles.countdownContainer}>
                 <div>
                     <span>{minuteLeft}</span>
@@ -51,7 +60,9 @@ export function Countdown() {
                     <span>{secondRight}</span>
                 </div>
             </div>
-            {Finished ? (
+
+
+            {hasFinished ? (
                 <button
                     disabled
                     className={styles.countdownButton}
@@ -74,6 +85,7 @@ export function Countdown() {
                             )}
                     </>
                 )}
+
         </div>
     );
 }
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -2,62 +2,67 @@ import { createContext, useState, ReactNode } from 'react';
 import challenges from '../../challenges.json';
 
 interface Challenge {
-    type: 'body' | 'eye';
-    description: string;
-    amount: number;
-}
-interface ChallengesContextData {
-    level: number;
-    currentExperience: number;
-    challengesCompleted: number;
-    activeChallenge: Challenge;
-    experienceToNextLevel: number;
-    levelUp: () => void;
-    startNewChallenge: () => void;
-    resetChallenge: () => void;
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
 }
 
-interface ChallengesProviderProps {
-    children: ReactNode
+interface ChallengeContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  activeChallenge: Challenge;
+  experienceToNextLevel: number;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
-export const ChallengesContext = createContext({} as ChallengesContextData);
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
+interface ChallengeProviderProps {
+  children: ReactNode;
+}
 
-    const [activeChallenge, setActiveChallenge] = useState(null);
+export const ChallengesContext = createContext({} as ChallengeContextData);
 
-    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
-    function levelUp() {
-        setLevel(level + 1);
-    }
+export function ChallengesProvider({ children }: ChallengeProviderProps) {
 
-    function startNewChallenge() {
-        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
 
-        const challenge = challenges[randomChallengeIndex];
+  const [activeChallenge, setActiveChallenge] = useState(null)
 
-        setActiveChallenge(challenge);
-    }
 
-    function resetChallenge() {
-        setActiveChallenge(null);
-    }
-    return (
-        <ChallengesContext.Provider value={{
-            level,
-            currentExperience,
-            challengesCompleted,
-            activeChallenge,
-            experienceToNextLevel,
-            levelUp,
-            startNewChallenge,
-            resetChallenge
-        }}>
-            {children}
-        </ChallengesContext.Provider>
-    );
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+  function levelUp() { }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
+
+  return (
+    <ChallengesContext.Provider
+      value={{
+        level,
+        levelUp,
+        currentExperience,
+        challengesCompleted,
+        startNewChallenge,
+        activeChallenge,
+        resetChallenge,
+        experienceToNextLevel
+      }}>
+      {children}
+    </ChallengesContext.Provider>
+  );
 }
